c333298e369cab727e87bbbaedf4520ac9af4110
Narek <nmeloyan777@gmail.com>
2023-10-07 13:52:25 +0400

    My first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+//     $var = "some text";
+//     $num = 123;
+
+// //  isset(),empty();
+
+// // $result = isset($var);
+// // echo $result;
+
+// // echo "<br>";
+
+// // $status = empty($nudm);
+// // echo $status;
+
+// $str = "";
+
+// if (isset($str)) {
+//     echo "Varible $str is exist". "<br>";
+// }
+
+// if (empty($str)) {
+//     echo "Varible $str is not exisist". "<br>";
+// }
+
+
+// echo gettype(isset($str));
+
+
+
+// HOMEWORK 
+
+// 1
+
+// $string = "";
+
+// echo isset($stirng);
+
+
+// 2 
+
+// $a = 1;
+// $b = 2;
+// $c = 3;
+
+// echo isset($a,$b,$c);
+
+
+// $con = new mysqli('localhost','root','','connect') or die("Uneble to exsits");
+
+// if($con) {
+//     $products = mysqli_query($con,"SELECT * FROM `users`"); 
+//     $products = mysqli_fetch_all($products);
+//     var_dump($products);    
+// }
---
+++ oop/main.php
@@ -0,0 +1,22 @@
+<?php
+
+//     class Person{
+//         public $age;
+//         public $name;
+//     }
+
+// $person = new Person();
+// $person->name = "Narek";
+// $person->age = 16;
+
+// echo $person->name . " - " . $person->age . "<br>";
+
+
+// $person1 = new Person();
+// $person1->name = "Armine";
+// $person->age = 16;
+
+// echo $person1->name. " - " . $person->age ;
+
+
+// homework 1 
